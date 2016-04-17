1d02bbdf491fb142570156e6afbda3b8a0245339
Heheggg <yxu8@stuy.edu>
2016-04-17 07:36:50 -0400

    help

--- userdata.py
+++ userdata.py
@@ -4,11 +4,11 @@ import json, time, main, book
 #[[isbn, date], [isbn2, date2], ...]
 #So an array of subarrays, where each subarray contains an isbn identifier, and the date it was added to this list
 
-with open('userdata.json') as f:
-    data = json.load(f)
 
 
 def addSavedBook(username, isbn):
+    with open('userdata.json') as f:
+        data = json.load(f)
     entry = [isbn, time.strftime("%x")]
     data[username]['savedBooks'].append(entry)
     with open('userdata.json', 'w') as f:
@@ -16,10 +16,14 @@ def addSavedBook(username, isbn):
 
 
 def getSavedBookList(username):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     return data[username]['savedBooks']
 
 
 def addDislikedBook(username, isbn):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     entry = [isbn, time.strftime("%x")]
     data[username]['dislikedBooks'].append(entry)
     with open('userdata.json', 'w') as f:
@@ -27,10 +31,14 @@ def addDislikedBook(username, isbn):
 
 
 def getDislikedBookList(username):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     return data[username]['dislikedBooks']
 
 
 def addLikedBook(username, isbn):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     entry = [isbn, time.strftime("%x")]
     data[username]['likedBooks'].append(entry)
     with open('userdata.json', 'w') as f:
@@ -38,19 +46,27 @@ def addLikedBook(username, isbn):
 
 
 def getLikedBookList(username):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     return data[username]['likedBooks']
 
 
 def addTag(username, key, value):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     data[username]['tagRanks'][key] = value
     with open('userdata.json', 'w') as f:
         json.dump(data, f)
 
-def getTagDict(username):
+def getTagDict(username): 
+    with open('userdata.json') as f:
+        data = json.load(f)   
     return data[username]['tagRanks']
 
 #returns a tag over the threshold, else returns None
 def getPopularTag(username):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     maxPop = 0
     popTag = None
     #NOT BEING USED
@@ -64,6 +80,8 @@ def getPopularTag(username):
 
 
 def getCurrentBook(username):
+    with open('userdata.json') as f:
+        data = json.load(f)
     if(data[username]['currentBook'] == { }):
         return { };
     retBook = book.Book()
@@ -72,25 +90,35 @@ def getCurrentBook(username):
 
 
 def changeCurrentBook(username, curBook):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     data[username]['currentBook'] = curBook.getAsDict();
     with open('userdata.json', 'w') as f:
         json.dump(data, f)
 
 def getNum(username):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     return data[username]['num']
 
 
 def changeNum(username, offset):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     data[username]['num'] = data[username]['num'] + offset
     with open('userdata.json', 'w') as f:
         json.dump(data, f)
 
 
 def getP(username):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     return data[username]["P"]
 
 
 def setP(username, newP):
+    with open('userdata.json') as f:
+        data = json.load(f)    
     data[username]["P"] = newP
     with open('userdata.json', 'w') as f:
         json.dump(data, f)
